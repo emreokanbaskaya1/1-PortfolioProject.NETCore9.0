6beed60da54b743c7422cfdde0bdfea2689db057
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-08-27 20:53:11 +0300

    8/27/2025-update

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Context;
+
+namespace Portfolio.Web.Controllers
+{
+    public class StatisticsController(PortfolioContext context) : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.projectCount = context.Projects.Count(); // Proje Sayısı
+            ViewBag.skillsAverage = context.Skills.Any() ? context.Skills.Average(x=>x.Percentage).ToString("00.00") : 0.0.ToString("00.00");
+            ViewBag.unreadMessageCount = context.UserMessages.Count(x => x.IsRead == false);
+            ViewBag.lastMessageOwner = context.UserMessages.OrderByDescending(x => x.UserMessageId).Select(x=>x.Name).FirstOrDefault();
+            return View(); 
+        }
+    }
+}
